e705567e8879c027efbdd6e080e0d4db8e24b432
ukonpower <obshi0120@icloud.com>
2019-03-30 18:24:15 +0900

    open時、再生

---
+++ Assets/Scripts/SystemController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SystemController : MonoBehaviour
+{
+
+    private Door door;
+    private VideoController vc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vc = GameObject.Find("VideoPlayer").GetComponent<VideoController>();
+        door = GetComponent<Door>();
+        door.onOpen += () => {
+            vc.PlayVideo();
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/VideoController.cs
+++ Assets/Scripts/VideoController.cs
@@ -1,18 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
-
+using UnityEngine.Video;
 public class VideoController : MonoBehaviour
 {
+    private VideoPlayer vp;
+    private string[] videoList;
+    private int lastVideo = -1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        vp = GetComponent<VideoPlayer>();
+        videoList = getVideoFiles();
+    }
+
+    string[] getVideoFiles(){
+        string basePath = Application.streamingAssetsPath + "/videos/";
+
+        DirectoryInfo dInfo = new DirectoryInfo(basePath);
+        FileInfo[] fInfo = dInfo.GetFiles("*.mp4");
+
+        string[] paths = new string[fInfo.Length];
+        for(int i = 0; i < fInfo.Length; i++)
+        {
+            paths[i] = basePath + fInfo[i].Name;
+        }
+        return paths;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void PlayVideo(){
+        int nextVideo = Random.Range(0,videoList.Length);
+
+        if(nextVideo == lastVideo){
+            nextVideo = (nextVideo + 1) % videoList.Length;
+        }
+
+        vp.url = videoList[nextVideo];
+        vp.Play();
+
+        Debug.Log("play:" + vp.url);
     }
 }
