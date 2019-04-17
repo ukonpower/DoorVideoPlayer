430dff3285e9a592e6b3084d2cd525519c340b89
505Mm <505mm@505Mm-no-Mac-mini-3.local>
2019-04-18 02:31:59 +0900

    バージョン変えた

--- Assets/Scripts/SystemController.cs
+++ Assets/Scripts/SystemController.cs
@@ -24,6 +24,8 @@ public class SystemController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.Escape)){
+            Application.Quit();
+        }
     }
 }
